12bc9a60987528d0571e0b806bd8f87a42f97074
martinthoresen <87776525+martinthoresen@users.noreply.github.com>
2022-09-28 01:36:30 +0200

    add carousel, add view more function, fix h1 margins

--- js/blog.js
+++ js/blog.js
@@ -1,23 +1,28 @@
-const url = "https://healthandfitness.flowerpower12394.one//wp-json/wp/v2/posts";
 const blogContainer = document.querySelector(".blogs-container");
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
+const loadMore = document.querySelector(".load-more-btn");
+const loader = document.querySelector(".loader");
+let currentPage = 0;
 
 async function getBlogs() {
+  currentPage++;
+  const url = `https://healthandfitness.flowerpower12394.one//wp-json/wp/v2/posts?per_page=10&page=${currentPage}`;
   try {
     const response = await fetch(url);
     const result = await response.json();
     const blogs = result;
-
-    console.log(blogs);
-
-    blogContainer.innerHTML = "";
+    const maxPages = response.headers.get("x-wp-totalpages");
+    if (Number(maxPages) === currentPage) {
+      loadMore.style.display = "none";
+    }
+    loader.style.display = "none";
 
     for (let i = 0; i < blogs.length; i++) {
       function createHtml(blogs) {
         blogContainer.innerHTML += `<div class="blog">
                                         <p>09.09.2022, Robert Thompson</p>
-                                        <img src="${blogs[i].better_featured_image.source_url}">
+                                        <img src="${blogs[i].better_featured_image.source_url}" alt="${blogs[i].better_featured_image.alt_text}">
                                         <h2>${blogs[i].title.rendered}</h2>
                                         <p>${blogs[i].excerpt.rendered}</p>
                                         <a href="blog-post.html?slug=${blogs[i].slug}">Read</a>
@@ -34,6 +39,8 @@ async function getBlogs() {
 }
 getBlogs();
 
+loadMore.addEventListener("click", getBlogs);
+/* HAMBURGER MENU */
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const url = "https://healthandfitness.flowerpower12394.one//wp-json/wp/v2/posts";
+const url = "https://healthandfitness.flowerpower12394.one//wp-json/wp/v2/posts?per_page=16";
 const blogContainer = document.querySelector(".latest-blogs-container");
 const hamburger = document.querySelector(".hamburger");
 const navbar = document.querySelector(".navbar");
@@ -38,3 +38,37 @@ getBlogs();
 hamburger.addEventListener("click", () => {
   navbar.classList.toggle("active");
 });
+
+var slidePosition = 1;
+carousel(slidePosition);
+
+function changeSlides(n) {
+  carousel((slidePosition += n));
+}
+
+function currentSlide(n) {
+  carousel((slidePosition += n));
+}
+
+function carousel(n) {
+  var i;
+  var blogPosts = document.getElementsByClassName("latest-blogs");
+  console.log(blogPosts);
+  var dots = document.getElementsByClassName("dots");
+
+  if (n > blogPosts.length) {
+    slidePosition = 1;
+  }
+  if (n < 0) {
+    slidePosition = blogPosts.length;
+  }
+  for (i = 0; i < blogPosts.length; i++) {
+    blogPosts[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" enable", "");
+  }
+  console.log(blogPosts[slidePosition - 1]);
+  blogPosts[slidePosition - 1].style.display = "block";
+  dots[slidePosition - 1].className += " enable";
+}
